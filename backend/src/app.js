810ec520a2601c752e0fa028248787fa8308f5e4
mario <mariopimenta86@gmail.com>
2025-12-09 18:06:43 -0300

    feat: add crud apis

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,9 @@ const app = express();
 app.use(express.json());
 
 import userRoute from './routes/user.route.js';
+import postRoute from './routes/post.route.js';
 
 app.use('/api/v1/users', userRoute);
+app.use('/api/v1/posts', postRoute);
 
 export default app;
